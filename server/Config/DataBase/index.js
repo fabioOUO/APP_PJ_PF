e823422b6e560e618fc10e932e9831947a598350
fabioOUO <fabiojbarbosa1@gmail.com>
2022-02-21 20:56:30 -0300

    Alteração em variaveis de conexão BD

--- server/Config/DataBase/index.js
+++ server/Config/DataBase/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config({ path: "./server/.env" });
 
 /* Configurações de conexão com o banco de dados */
-
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DATA_BASE,
